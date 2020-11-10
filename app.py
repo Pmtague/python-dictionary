59276598f69200a4ad34266da4364c7a81df7de2
Penny Tague <pmtague9@gmail.com>
2020-11-10 10:33:36 -0800

    Initial commit, dictionary returns exact and similar matches

---
+++ app.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+# Library to pull similar words to use input
+import difflib
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
+# Save the data as a python dictionary
+data = json.load(open("data.json"))
+
+# Takes the user input, normalizes it, and returns the definitions
+
+
+def get_definition(word):
+
+    word = word.lower()
+
+    if word in data:
+        return data[word]
+        # TODO: Did you mean (list of matches)?
+        # Allow user to select one of the matches
+        # Return the word and definition the user chooses
+    else:
+        matches = get_close_matches(word, data.keys())
+
+        if matches[0] in data:
+            return data[matches[0]]
+        else:
+            print("{} is not in the dictionary! Please double check your spelling.".format(
+                word.capitalize()))
+
+
+# Get user input
+word = input("Enter word: ")
+
+# Clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Print to the terminal in a human readable fashion
+defined = get_definition(word)
+
+print("{}:\n".format(word.capitalize()))
+
+print_count = 1
+for definition in defined:
+    print("{}. {}\n".format(print_count, definition))
+    print_count += 1
